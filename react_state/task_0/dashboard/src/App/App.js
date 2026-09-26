6ffe3c897ef59406acfc3383a34a6dc6b3929374
evand3rr <e.manna@alustudent.com>
2026-09-26 14:01:03 +0200

    updated React State

---
+++ react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,106 @@
+import './App.css';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import PropTypes from 'prop-types';
+
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { getLatestNotification } from '../utils/utils';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+    };
+    this.handleKeydown = this.handleKeydown.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+  }
+
+  // Lifecycle Methods
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  // Handle Log out
+  handleKeydown(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  render() {
+    const { isLoggedIn, logOut } = this.props;
+    const { displayDrawer } = this.state;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const htmlObj = getLatestNotification();
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New course available' },
+      { id: 3, type: 'urgent', html: htmlObj },
+    ]
+
+    return (
+      <>
+        <Notifications
+          displayDrawer={ displayDrawer }
+          handleDisplayDrawer={ this.handleDisplayDrawer }
+          handleHideDrawer={ this.handleHideDrawer }
+          listNotifications={ listNotifications }
+        />
+        <div className="App">
+          <Header />
+          { isLoggedIn ?
+          <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={ listCourses } />
+          </BodySectionWithMarginBottom>
+           :
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
+          }
+          <BodySection title="News from the School">
+            <p>Graduation date is January 28th!</p>
+          </BodySection>
+          <Footer />
+        </div>
+      </>
+    )
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default hot(module)(App);
---
+++ react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './Notifications.css';
+import close_icon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+
+class Notifications extends React.Component {
+    constructor(props) {
+        super(props);
+        this.markAsRead = this.markAsRead.bind(this);
+    }
+
+    markAsRead(id) {
+        console.log(`Notification ${id} has been marked as read`);
+    }
+
+    // Lifecycle Methods
+    shouldComponentUpdate(updatedProps, updatedState) {
+        if (updatedProps.displayDrawer !== this.props.displayDrawer)
+            return true;
+        if (updatedProps.listNotifications.length > this.props.listNotifications.length)
+            return true;
+        return false;
+    }
+
+    render() {
+        const { displayDrawer, handleDisplayDrawer, handleHideDrawer, listNotifications } = this.props;
+
+        return (
+            <>
+            <div className="menuItem" onClick={ handleDisplayDrawer }>
+                Your notifications
+            </div>
+            { displayDrawer &&
+                <div className="Notifications">
+                    { listNotifications.length > 0 ? (
+                        <>
+                            <button
+                                aria-label="Close"
+                                onClick={ handleHideDrawer }
+                            ><img
+                                    src={ close_icon }
+                                    alt="Close"
+                                />
+                            </button>
+                            <p>Here is the list of notifications</p>
+                            <ul>
+                                { listNotifications.map((notification) => (
+                                    <NotificationItem
+                                        key={ notification.id }
+                                        type={ notification.type }
+                                        value={ notification.value }
+                                        html={ notification.html }
+                                        markAsRead={() => this.markAsRead(notification.id)}
+                                    />
+                                )) }
+                            </ul>
+                        </>
+                    ) : <p>No new notification for now</p> }
+                </div>
+            }
+            </>
+        )
+    }
+}
+
+Notifications.propTypes = {
+    displayDrawer: PropTypes.bool,
+    handleDisplayDrawer: PropTypes.func,
+    handleHideDrawer: PropTypes.func,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+    displayDrawer: false,
+    handleDisplayDrawer: () => {},
+    handleHideDrawer: () => {},
+    listNotifications: [],
+}
+
+export default Notifications;
